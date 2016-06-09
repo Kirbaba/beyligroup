e0f668091d0360f1a397a564b5ed23c96a3bbd88
Vladislav <v.kirbaba@gmail.com>
2016-06-09 17:26:14 +0300

    Меню

--- app/js/script.js
+++ app/js/script.js
@@ -8,4 +8,18 @@ jQuery(document).ready(function($) {
 		$('.page__tabcontent').hide();
 		$("."+ target).show();
 	});
+
+	$('.home__entry_logo').click(function(event) {
+		event.preventDefault();
+		$('.enter_screen').hide();
+		$('.home__projects').hide();
+		$('.menu').show();
+	});
+
+	$('.menu_close').click(function(event) {
+		event.preventDefault();
+		$('.menu').hide();
+		$('.enter_screen').show();
+		$('.home__projects').show();
+	});
 });
